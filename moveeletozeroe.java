64dbdb6821405768263445f1a07ffd7ec62f5c0d
vinayaktripathi1279 <vinayaktripathi1279@gmail.com>
2025-12-22 14:25:30 +0530

    Kadane algorithm

---
+++ moveeletozeroe.java
@@ -0,0 +1,28 @@
+public class moveeletozeroe{
+
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+
+      
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        // print array
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
